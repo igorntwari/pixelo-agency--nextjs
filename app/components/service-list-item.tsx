a82d4124fcc565f799a3daec3a8dacf8259ce570
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-28 21:32:05 +0200

    feat: first steps of service-list-item

---
+++ app/components/service-list-item.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import classNames from "classnames";
+import { Roboto_Serif } from "next/font/google";
+import { CSSProperties, PropsWithChildren, useRef } from "react";
+import { useIntersectionObserver } from "usehooks-ts";
+import { Edit, PenTool, PieChart, Layout, Users, Award } from "react-feather";
+import { Service } from "../sections/services";
+
+const robotoSerif = Roboto_Serif({ subsets: ["latin"], weight: "500" });
+
+const ServicesIcons = [Edit, PenTool, PieChart, Layout, Users, Award];
+
+export default function ServiceListItem({
+  id,
+  title,
+  description,
+  children,
+}: PropsWithChildren<Service>) {
+  const reference = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(reference, {});
+  const isVisible = !!entry?.isIntersecting;
+
+  const Icon = ServicesIcons[id];
+
+  return (
+    <div
+      className={classNames("flex flex-col gap-3 group duration-500", {
+        "animate-focus": isVisible,
+      })}
+      style={{ "--order": id } as CSSProperties}
+      ref={reference}
+    >
+      <div className="rounded-full bg-white mr-auto p-5">
+        <Icon className="size-5 animate-focus-icon" />
+      </div>
+      <h3 className={classNames(robotoSerif.className, "font-medium")}>
+        {title}
+      </h3>
+      <p className="text-sm">{description}</p>
+    </div>
+  );
+}
