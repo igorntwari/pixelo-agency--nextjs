080b5d501b4591ff06eda72045784e5e944959bd
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-28 21:33:22 +0200

    refactor: add services section in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/hero";
+import { Services } from "./sections/services";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
