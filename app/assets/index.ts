14565c478b8ccaa116e03cea4ca3a99e25f3c4ef
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-28 08:54:33 +0200

    feat: export all assets in one file

---
+++ app/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as Stars } from "./svg/stars";
+export { default as Logo } from "./svg/logo";
+export { default as Leafe } from "./svg/leafe-logo";
+export { default as Mindfullness } from "./svg/mindfullness-logo";
+export { default as Greenish } from "./svg/greenish-logo";
+export { default as Automation } from "./svg/automation-logo";
+
+export { default as heroImage } from "./images/hero.png";
