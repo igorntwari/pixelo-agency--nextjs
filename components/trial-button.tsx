abb67a9869a4b1c0512b05f126e86673f992e40b
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-29 08:56:53 +0200

    feat: create trial-button component

---
+++ components/trial-button.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { ChevronRight } from "react-feather";
+import classNames from "classnames";
+
+interface TrialButtonProperties {
+  dark: boolean;
+  className: string;
+  link?: string;
+}
+
+export default function TrialButton({
+  dark,
+  link,
+  className,
+}: TrialButtonProperties) {
+  return (
+    <Link
+      href={link ?? "/"}
+      className={classNames(
+        "flex text-sm sm:text-base items-center rounded-full p-1 sm:p-2 pr-4 gap-3",
+        className,
+        {
+          "bg-white text-neutral-950": !dark,
+          "bg-neutral-950 text-white": dark,
+        },
+      )}
+    >
+      <ChevronRight
+        className={classNames("rounded-full size-5 sm:size-auto", {
+          "bg-black text-white": !dark,
+          "bg-white text-neutral-950": dark,
+        })}
+      />
+      <span>Start your free trial</span>
+    </Link>
+  );
+}
