08f8224016a9362f955577dffda4e96fc55514a8
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-29 15:35:44 +0200

    feat: projects images

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -24,3 +24,14 @@ export { default as heroImage } from "./images/hero.png";
 export { default as team1 } from "./images/team-1.jpg";
 export { default as team2 } from "./images/team-2.jpg";
 export { default as team3 } from "./images/team-3.jpg";
+
+export { default as project1 } from "./images/project-1.png";
+export { default as project1_1 } from "./images/project-1-1.png";
+export { default as project1_2 } from "./images/project-1-2.png";
+export { default as project1_3 } from "./images/project-1-3.png";
+
+export { default as project2 } from "./images/project-2.png";
+export { default as project3 } from "./images/project-3.png";
+export { default as project4 } from "./images/project-4.png";
+export { default as project5 } from "./images/project-5.png";
+export { default as project6 } from "./images/project-6.png";
