12e9ae9564b8f2d9dbb42ca76a2cae41c6fa460c
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-29 14:48:41 +0200

    refactor: use process section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AboutUs from "./sections/about-us";
 import Hero from "./sections/hero";
 import Services from "./sections/services";
+import Process from "./sections/process";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Services />
       <AboutUs />
+      <Process />
     </>
   );
 }
