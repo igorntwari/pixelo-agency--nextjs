24622d1fb8e3338798623fae65aa3861f6465597
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-29 13:10:01 +0200

    feat: use about us section in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import AboutUs from "./sections/about-us";
 import Hero from "./sections/hero";
-import { Services } from "./sections/services";
+import Services from "./sections/services";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
+      <AboutUs />
     </>
   );
 }
