ccb3a083c738d23e485631e25e3a7a15a3a4f322
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-29 15:37:05 +0200

    feat: use showcase section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AboutUs from "./sections/about-us";
 import Hero from "./sections/hero";
 import Services from "./sections/services";
 import Process from "./sections/process";
+import ShowCase from "./sections/showcase";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Services />
       <AboutUs />
       <Process />
+      <ShowCase />
     </>
   );
 }
