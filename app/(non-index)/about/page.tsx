8d7bd1df16095d3e1e58cf0e8cce37daa5d75810
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-31 11:48:16 +0200

    feat: add title on about us page

--- app/(non-index)/about/page.tsx
+++ app/(non-index)/about/page.tsx
@@ -1,8 +1,13 @@
+import { Metadata } from "next";
 import Contact from "./sections/contact";
 import Counter from "./sections/counter";
 import HeroAbout from "./sections/hero-about";
 import Mission from "./sections/mission";
 import Team from "./sections/team";
+
+export const metadata: Metadata = {
+  title: "About us",
+};
 
 export default function About() {
   return (
