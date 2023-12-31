5dc7166254eb7979541f7db6dc608fef6b22dad0
Lionel Kaneza <lionkaneza@gmail.com>
2023-12-31 14:58:40 +0200

    feat: create projects page

---
+++ app/(non-index)/projects/page.tsx
@@ -0,0 +1,9 @@
+import Portfolio from "./sections/portfolio";
+
+export default function Projects() {
+  return (
+    <main className="*:my-10 width-responsive">
+      <Portfolio />
+    </main>
+  );
+}
