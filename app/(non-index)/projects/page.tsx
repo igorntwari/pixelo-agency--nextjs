2e1a1ab26747de5ee10f6b230b002d28faef3fdd
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-01 14:00:57 +0200

    feat: add title on projects page

--- app/(non-index)/projects/page.tsx
+++ app/(non-index)/projects/page.tsx
@@ -1,6 +1,11 @@
 import Contact from "@/components/sections/contact";
 import Portfolio from "./sections/portfolio";
 import ShowCase from "./sections/showcase";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Our Projects",
+};
 
 export default function Projects() {
   return (
