e03d5da217f2415f95f5464c9f23da6a5f4e56d9
Lionel Kaneza <lionkaneza@gmail.com>
2024-01-05 20:38:43 +0200

    refactor: change size of 3xl screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
         focus: "focus 1s ease-in-out calc(var(--order) * 1000ms) forwards",
       },
       screens: {
-        "3xl": "1600px",
+        "3xl": "1700px",
         "semi-md": "870px",
       },
     },
